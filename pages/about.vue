ce75dd8ffadefb46232e6ef73dcd2b5e22bd5553
deoj <deoj.phil@yahoo.ca>
2022-11-30 00:05:10 -0700

    change v-on to shorthand

--- pages/about.vue
+++ pages/about.vue
@@ -7,12 +7,11 @@
   </main>
 
   <!-- v-on example-->
-  <p>v-on Example</p>
   <div class="text-center mt-20">
     <p>The Current Number is:</p>
     <h3 class="text-2xl">{{ number }}</h3>
-    <button v-on:click="number++" class="text-xl font-bold mr-5">+</button>
-    <button v-on:click="number--" class="text-xl font-bold ml-5">-</button>
+    <button @:click="number++" class="text-xl font-bold mr-5">+</button>
+    <button @:click="number--" class="text-xl font-bold ml-5">-</button>
   </div>
 </template>
 
